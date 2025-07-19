41026bdaa67ecc8e41ac1b1f77b900b316b89731
zhouwei <zhouwei@chances.com.cn>
2025-07-19 21:23:04 +0800

    fix(linear regression): 正态分布

--- tests/linear-regression/test_vector_acclerate.py
+++ tests/linear-regression/test_vector_acclerate.py
@@ -1,7 +1,7 @@
 import unittest
 import math
 import time
-import numpy
+import numpy as np
 import torch
 from d2l import torch as d2l
 
@@ -30,6 +30,21 @@ class TestVectorAccelerate(unittest.TestCase):
         # 验证结果循环比矢量化操作慢
         self.assertTrue(loop_time > vectorized_time)
 
+    def test_normal_distribution(self):
+        """测试正态分布"""
+        # 再次使用numpy进行可视化
+        x = np.arange(-7, 7, 0.01)
+        # 均值和标准差对
+        params = [(0, 1), (0, 2), (3, 1)]
+        # 画图
+        d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x',
+                 ylabel='p(x)', figsize=(4.5, 2.5),
+                 legend=[f'mean {mu}, std {sigma}' for mu, sigma in params])
+        # 显示图片
+        d2l.plt.show()
+
+        # 可以发现 mu 值影响高度、sigma 影响水平位移
+
 
 class Timer:
     """记录多次运行时间"""
@@ -57,7 +72,13 @@ class Timer:
 
     def cumsum(self):
         """返回累计时间"""
-        return numpy.array(self.times).cumsum().tolist()
+        return np.array(self.times).cumsum().tolist()
+
+
+# 计算正态分布的概率密度函数
+def normal(x, mu, sigma):
+    p = 1 / math.sqrt(2 * math.pi * sigma ** 2)
+    return p * np.exp(-0.5 / sigma ** 2 * (x - mu) ** 2)
 
 
 if __name__ == "__main__":
